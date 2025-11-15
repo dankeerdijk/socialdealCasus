ea1c23a20730a2af166368f1cad6b77171a2fcc5
Dan Keerdijk <dankeerdijk@MacBook-Air-van-Dan.local>
2025-11-15 21:14:42 +0100

    first-commit

---
+++ Socialdealcasus/Socialdealcasus/SocialdealcasusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialdealcasusApp.swift
+//  Socialdealcasus
+//
+//  Created by Dan Keerdijk on 15/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SocialdealcasusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Socialdealcasus/SocialdealcasusTests/SocialdealcasusTests.swift
@@ -0,0 +1,16 @@
+//
+//  SocialdealcasusTests.swift
+//  SocialdealcasusTests
+//
+//  Created by Dan Keerdijk on 15/11/2025.
+//
+
+import Testing
+
+struct SocialdealcasusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
